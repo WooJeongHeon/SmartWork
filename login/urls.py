b1077fa4358ca92c89d1f6277db48adf88d5759c
JeongHeon Woo <email@woojeongheon.com>
2019-10-21 16:00:46 +0000

    made login forms and models
    Signed-off-by: JeongHeon Woo <email@woojeongheon.com>

--- login/urls.py
+++ login/urls.py
@@ -19,6 +19,7 @@ from . import views
 urlpatterns = [
     path('sign_in/', views.sign_in),
     path('sign_up/', views.sign_up),
+    path('login/', views.login),
 
 
 ]
--- login/views.py
+++ login/views.py
@@ -12,3 +12,21 @@ def sign_up(request):
         request,
         'sign_up.html'
     )
+# =============================================== 밑에는 네이버 참고 복붙
+
+from django.shortcuts import render, redirect, HttpResponse
+from .models import Users
+from django.contrib.auth.hashers import make_password, check_password
+from .forms import LoginForm
+
+def login(request):
+    if request.method =="POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'login.html', {'form':form})  
+
