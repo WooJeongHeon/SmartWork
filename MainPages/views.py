41ef8bdb48c60c5eb70919ac4bfa58d8b88e32af
JeongHeon Woo <email@woojeongheon.com>
2019-10-21 14:18:38 +0000

    made SmartWork Project
    made app: basecamp, MainPages, MainPage
    
    made venv
    
    add style files: element.ui
    
    Signed-off-by: JeongHeon Woo <email@woojeongheon.com>

---
+++ MainPages/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+
+
+def index(request):
+    # return redirect('/blog/') # https://programtest11.run.goorm.io/blog/ 로 리다이렉트
+    return render(
+        request,
+        'wjh_main_site.html'
+    )
+
+
+def about_me(request):
+    return render(
+        request,
+        'about_me.html'
+    )
+
+def robots(request):
+    return render(
+        request,
+        'robots.txt'
+    )
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/workspace/SmartWork_V0/SmartWork_practice/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
