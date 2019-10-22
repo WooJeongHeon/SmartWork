60fe00ea14ff231e6f474ebd0783dd09d63d7aa7
JeongHeon Woo <email@woojeongheon.com>
2019-10-22 01:22:32 +0000

    ordering by order in MsgBoards
    Signed-off-by: JeongHeon Woo <email@woojeongheon.com>

--- AdminPages/models.py
+++ AdminPages/models.py
@@ -7,7 +7,7 @@ from markdownx.utils import markdown # markdownx 모듈 사용
 class MsgBoards(models.Model):
     name = models.CharField(max_length=25, unique=True) # 글자수 제한: 25, 중복 방지
     description = models.TextField(blank=True) # 게시판에대한 설명, blank=True: 안써도 됨
-
+    order = models.CharField(max_length=25, unique=True)
     slug = models.SlugField(unique=True, allow_unicode=True) # allow_unicode=True: 한글 가능, 사용자가 게시판 name을 쓰면 자동으로 slug 만들어지게 할거임.(url에 게시판 이름 구성하려고)
 
     def __str__(self):
@@ -18,3 +18,4 @@ class MsgBoards(models.Model):
 
     class Meta:
         verbose_name_plural = 'MsgBoards'
+        ordering = ['order', ] # order 순으로 오름차순 정렬
